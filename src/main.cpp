32ede2ee6d01d87052f9af2378a657a1d314284b
=kanaxz <kanaxstratz@gmail.com>
2017-01-07 15:41:52 +0000

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -14,29 +14,29 @@ int main(void)
   // Back right motor 
   Motor			*motorBR;
   // Last key hit
-  int			key;
-  // Servo mastah file descriptor
-  std::filebuf		*fb;
-  // Output stream servomaster
-  std::istream		*is;
+
   
   motorFL = new Motor(17); // Instanciates the motor from GPIO 17
   motorFR = new Motor(17); // Instanciates the motor from GPIO 17
   motorBL = new Motor(17); // Instanciates the motor from GPIO 17
   motorBR = new Motor(17); // Instanciates the motor from GPIO 17
-  // Open the file
-  fb.open("/dev/servomaster", std::ios::in);
-  // Output stream instanciation
-  is = new istream(fb);
-  is->write("1=25%");
-  
-  // Main infinite loop used to execute keystroke-based actions
-  /*  while (1)
-    {
 
-      usleep(20000);
-    }
-  */
+  // Open the device in non-blocking mode
+  int fd = open("/dev/ttyPA1", O_RDWR | O_NONBLOCK);
+  if(fd < 0)
+      ;  // handle error
+
+  // Try to write some data
+  ssize_t written = write(fd, "1=25%", 5);
+  if(written >= 0)
+      ;  // handle successful write (which might be a partial write!)
+  else if(errno == EWOULDBLOCK)
+      ;  // handle case where the write would block
+  else
+      ;  // handle real error
+
+  // Reading data is similar
+    close(fd);
 
   // DESTROY TIME
   delete motorFL;
