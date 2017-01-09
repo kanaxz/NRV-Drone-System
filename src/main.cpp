42a5b11427a88689deb55801251f7a2eb4cec679
kanaxz <kanaxstratz@gmail.com>
2017-01-09 15:05:11 +0100

    Change main

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,7 @@
 
 int main(void)
 {
-  // Front left motor 
-  Motor			*motorFL;
-  // Front right motor 
-  Motor			*motorFR;
-  // Back left motor 
-  Motor			*motorBL;
-  // Back right motor 
-  Motor			*motorBR;
-  // Last key hit
-
-  
-  motorFL = new Motor(1); // Instanciates the motor from GPIO 17
-  motorFR = new Motor(17); // Instanciates the motor from GPIO 17
-  motorBL = new Motor(17); // Instanciates the motor from GPIO 17
-  motorBR = new Motor(17); // Instanciates the motor from GPIO 17
-
-  // Open the device in non-blocking mode
-  
-
-  motorFL->setValue(20);
- 
-  
- 
-
-  // DESTROY TIME
-  delete motorFL;
-  delete motorFR;
-  delete motorBL;
-  delete motorBR;
+  Brain brain;
 
   return (0);
 }
