7d0a9e154ceb597ce43d5e742a7db828150ca6c9
kanaxz <kanaxstratz@gmail.com>
2017-01-18 21:57:36 +0100

    mpu6050 test

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,31 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "MPU6050.h"
+#include <iomanip>
 
 int main(void)
 { 
   
-  Brain brain;
+  //Brain brain;
   // Open the device in non-blocking mode
   
-  
+  MPU6050 imu;
+  imu.bypassDrift();
+  float ypr[3];
+  while(true) {
+        if(imu.getFIFOCount() > 42) {
+            imu.getYawPitchRoll(ypr);
+            std::cout << std::fixed << std::setprecision(3) << "YAW:"<<ypr[0]<<std::endl;
+        }
+  }
 
   
  
   
  
 
-  // DESTROY TIME
-  delete motorFL;
-  delete motorFR;
-  delete motorBL;
-  delete motorBR;
+
 
   return (0);
 }
